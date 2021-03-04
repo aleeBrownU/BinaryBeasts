387d35467ae770d72f72790ca0e91317a8cee593
alee <alee@localhost>
2021-03-03 22:04:13 -0500

    loads files

--- Analysis.m
+++ Analysis.m
@@ -1,5 +1,14 @@
 clearvars;
 close all;
 
-pyr_gids = 170:269; %stuff
-bask_gids = 135:169;
+cell_id = 135:269;
+
+
+
+for input_file_number = 2:12
+number_str = num2str(input_file_number);
+spike_times = readmatrix(strcat('Gamma',number_str,'.txt'));
+
+
+
+end

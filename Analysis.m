bf94ee8a244b07e7ef68c9883fa552cf6c9dd34f
alee <alee@172.16.105.227>
2021-03-04 22:38:47 -0500

    Added plot

--- Analysis.m
+++ Analysis.m
@@ -17,7 +17,7 @@ for input_file_number = 2:12 %Need to run code for each file. This for loop runs
     sample_isi_avg = [];
     sample_isi_std = [];
     
-    %{
+    
     
     % find spikes corresponding to each pyramidal cell
     cell_spikes = cell(length(cell_id),1); % matrix of cells where each item contains the list of spikes for a given cell
@@ -36,26 +36,23 @@ for input_file_number = 2:12 %Need to run code for each file. This for loop runs
     
     isi_std = [isi_std, std(isis_spk)];
     
-    %}
     
-    for num_samples = 1:1
+    
+    for num_samples = 1:20
         rand_id = randi([135 269],1,1000);
         for idx=1:length(cell_id)
             current_id = cell_id(idx); % the current ID
             sel = spike_file(:,2)==current_id; % select indicies within spike_file that correspond to the current ID
             current_times = spike_file(sel,1); % select the times of all spikes that correspond to the current ID
             curr_isi = diff(current_times); % calculate isi values
-            isis_spk = [isis_spk;curr_isi]; %appends to isis_spk the recently calculated isi
+            
             if any(rand_id(:) == current_id) %asks if any of the random sample ID's
                 isi_rand = [isi_rand, curr_isi'];
             end
         end
         sample_isi_avg = [sample_isi_avg, mean(isi_rand)]; %compiles all of the mean ISIs for the random sample
         sample_isi_std = [sample_isi_std, std(isi_rand)]; %compiles all of the standard deviations
-        
-        isi_avg = [isi_avg, mean(isis_spk)];  %compiles all of the mean ISIs
-    
-        isi_std = [isi_std, std(isis_spk)];     %compiles all of the std ISIs
+       
     
     end
     
@@ -66,10 +63,14 @@ data_pop = [2:12 ; isi_avg]';
 
 figure
 scatter(data_pop(:,1),data_pop(:,2));
+hold on
+gamma_number = data_pop(:,1);
+plot_avg_isi = data_pop(:,2);
+plot(fitlm(data_pop(:,1),data_pop(:,2)));
 title('Plot of Different Gammas and Average ISI');
 
 
-%{
+
 disp('isi avg');
 disp(isi_avg);
 disp('isi std');
@@ -78,4 +79,4 @@ disp('sample isi avg');
 disp(sample_isi_avg);
 disp('sample isi std');
 disp(mean(sample_isi_std));
-%}
+

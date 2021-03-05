6c5eeb9fd78e9e5fce74b617f52adcccdc60c5e7
alee <alee@localhost>
2021-03-04 21:09:31 -0500

    implemented  sampling for bootstrapping

--- Analysis.m
+++ Analysis.m
@@ -12,6 +12,10 @@ spike_file = readmatrix(strcat('Gamma',number_str,'.txt')); %Loads file (first c
 %put code here
 
 isis_spk = []; % column vector for storing the ISI value for each cell to be averaged later
+isi_rand = [];
+rand_id = randi([135 269],1,20);
+sample_isi_avg = [];
+
 
 % find spikes corresponding to each pyramidal cell
 cell_spikes = cell(length(cell_id),1); % matrix of cells where each item contains the list of spikes for a given cell
@@ -19,14 +23,21 @@ for idx=1:length(cell_id)
     current_id = cell_id(idx); % the current ID
     sel = spike_file(:,2)==current_id; % select indicies within spike_file that correspond to the current ID
     current_times = spike_file(sel,1); % select the times of all spikes that correspond to the current ID
-    curr_isis = diff(current_times); % calculate isi values
+    curr_isi = diff(current_times); % calculate isi values
     
-    isis_spk = [isis_spk;curr_isis]; %appends to isis_spk the recently calculated isi
+    isis_spk = [isis_spk;curr_isi]; %appends to isis_spk the recently calculated isi
+    if any(rand_id(:) == current_id) 
+        isi_rand = [isi_rand, curr_isi'];
+    end
+    
+    
 end
 
 isi_avg = [isi_avg, mean(isis_spk)]; 
+sample_isi_avg = [sample_isi_avg, mean(isi_rand)];
 
 
 end
 
 disp(isi_avg);
+disp(sample_isi_avg);
